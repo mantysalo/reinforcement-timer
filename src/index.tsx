11aa72b71528410b5fa62a4071cff0c4003f47bd
Riku Mäntysalo <riku.mantysalo@gmail.com>
2019-12-19 18:19:21 +0200

    add global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import normalize from 'styled-normalize';
+import { StylesProvider } from '@material-ui/styles';
+import { createGlobalStyle } from 'styled-components';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const GlobalStyles = createGlobalStyle`
+${normalize}
+body {
+font-family: Helvetica, Sans-Serif;
+}
+`
+
+ReactDOM.render(
+    <StylesProvider injectFirst>
+      <GlobalStyles/>
+        <App />
+    </StylesProvider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
